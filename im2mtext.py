52a031a28236cabee02ea52207de1f57ecd809b3
Mikoto Suo <suomikoto@outlook.com>
2018-11-10 21:56:25 +0800

    add hist eq

--- im2mtext.py
+++ im2mtext.py
@@ -2,11 +2,26 @@ from matplotlib import image as mpimg, pyplot as plt
 import numpy as np
 import cv2
 from pathlib import Path
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', help='file name of the input image')
+parser.add_argument('-C', '--charlist', default='@W#$OEXC[(/?=^~_.` ',
+                    help='character list used to print the output "image"')
+parser.add_argument('-H', '--height', type=int, default=100,
+                    help='number of text lines in the output (default=100)')
+parser.add_argument('-E', '--equalize', type=bool, action='store_true',
+                    help='equalize the histogram')
+
+
+def imshow(im):
+    plt.imshow(255 - im, cmap='Greys')
+    plt.show()
 
 
 def im2char(im, charlist, dsize):
     im = cv2.resize(im, dsize=dsize, interpolation=cv2.INTER_AREA)
-    im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
     length = len(charlist) - 1
     im = np.int32(np.round(im / 255 * length))
     output = []
@@ -23,9 +38,12 @@ def im2char(im, charlist, dsize):
 if __name__ == '__main__':
     path = Path('harbour.jpg')
     im = cv2.imread(str(path))
+    im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    im = cv2.equalizeHist(im)
+    imshow(im)
     height, width, *_ = im.shape
     charlist = '@W#$OEXC[(/?=^~_.` '
-    output_height = 125
+    output_height = 100
     output_width = round(width * 1.865 * output_height / height)
     # output_height = round(height / 1.865 * output_width / width)
     output = im2char(im, charlist, (output_width, output_height))
